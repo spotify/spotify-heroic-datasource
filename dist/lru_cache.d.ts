388b5c0bf79a7344d5f08d094838aabe2c426c83
William Huba <whuba@spotify.com>
2018-08-30 12:08:52 -0400

    Have travis build and upload the plugin.

--- dist/lru_cache.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class LruCache<T> {
-    private values;
-    private maxEntries;
-    get(key: string): T;
-    has(key: string): boolean;
-    put(key: string, value: T): void;
-}
--- dist/time_range.ts
+++
@@ -1,7 +0,0 @@
-export default class TimeRange {
-  public type: string;
-  public unit: string;
-  public value: number;
-  public start: number;
-  public end: number;
-}
