3ec55ac157b32c2136e942fdde3998a4a576c37a
Ryan Vazquez <ryanv@spotify.com>
2020-01-17 16:14:53 -0500

    update congfig with setup files

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   "roots": [
     "<rootDir>/src"
   ],
+  "setupFiles": [
+    "<rootDir>src/specs/test-setup/jest-setup.ts"
+  ],
   "testMatch": [
     "**/?(*.)+(spec|test).+(ts|tsx|js)"
   ],
