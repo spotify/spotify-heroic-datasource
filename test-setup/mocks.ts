fd330e3e23e13fac9dddef70d301fd29e2f39659
Ryan Vazquez <ryanv@spotify.com>
2020-01-29 13:49:38 -0500

    update testing dependencies

--- test-setup/mocks.ts
+++ test-setup/mocks.ts
@@ -1,5 +1,7 @@
 export let templateSrvMock = {
-  replace: jest.fn().mockImplementation(query => query)
+  replace: jest.fn().mockImplementation(query => query),
+  replaceWithText: jest.fn(n => n),
+  getAdhocFilters: jest.fn().mockReturnValue([])
 };
 
 export let backendSrvMock = {
@@ -13,6 +15,11 @@ export let datasourceSrvMock = {
 
 export let timeSrvMock = {
   timeRange: jest.fn().mockReturnValue({ from: '', to: '' })
+};
+
+export let uiSegmentSrvMock = {
+  newPlusButton: jest.fn(),
+  newSegment: jest.fn()
 };
 
 const defaultExports = {
